e2f15c69e6f7acdbb8a43871ec9f3ac0cd79f3dc
Alexander Richterich <richterich@yahoo.com>
2020-12-28 21:52:30 +0200

    Fetch a random background photo with the credits

--- src/index.js
+++ src/index.js
@@ -7,5 +7,43 @@ import Phaser from 'phaser';
 import GameConfig from './GameConfig';
 
 window.addEventListener('load', () => {
-    const game = new Phaser.Game(GameConfig);
+    let imageUrl =
+        'radial-gradient(circle at center, #302b63, #24243e, #0f0c29)';
+    let body = document.getElementsByTagName('body')[0];
+
+    fetch(
+        'https://localhost:8443/bg?searchterm=abstract&orientation=landscape&size=regular'
+    )
+        .then((response) => {
+            if (response.status !== 200) {
+                return response.status;
+            }
+            return response.json();
+        })
+        .then((backgroundMeta) => {
+            if (backgroundMeta.background) {
+                body.style.backgroundImage = `url(${backgroundMeta.background})`;
+                if (backgroundMeta.description) {
+                    body.title = backgroundMeta.description;
+                }
+                let profile_url = document.createElement('a');
+                profile_url.href = backgroundMeta.profile_url;
+                profile_url.target = '_blank';
+                profile_url.rel = 'noopener noreferrer';
+                let username = document.createElement('strong');
+                username.innerText = '@' + backgroundMeta.username;
+                let credit = document.getElementById('credit');
+                credit.textContent = 'background photo by ';
+                profile_url.appendChild(username);
+                credit.appendChild(profile_url);
+            } else {
+                body.style.backgroundImage = imageUrl;
+            }
+            const game = new Phaser.Game(GameConfig);
+        })
+        .catch((err) => {
+            body.style.backgroundImage = imageUrl;
+            const game = new Phaser.Game(GameConfig);
+            throw err;
+        });
 });
