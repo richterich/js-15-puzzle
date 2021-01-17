7f83d89f7897819286d2e97e24cc9079bc67314c
Alexander Richterich <richterich@yahoo.com>
2021-01-17 12:03:23 +0200

    Format code

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
-module.exports = api => {
-  const isTest = api.env('test');
-  console.log(`[INFO]\tConfig was run on ${isTest ? 'test' : 'NOT test'} environment!`);
-  return {
-    presets: [
-      ["@babel/env", {
-        targets: {
-          browsers: [
-            ">0.25%",
-            "not ie 11",
-            "not op_mini all"
-          ]
-        },
-        modules: isTest ? "auto" : false
-      }]
-    ]
-  };
+module.exports = (api) => {
+    const isTest = api.env('test');
+    console.log(
+        `[INFO]\tConfig was run on ${isTest ? 'test' : 'NOT test'} environment!`
+    );
+    return {
+        presets: [
+            [
+                '@babel/env',
+                {
+                    targets: {
+                        browsers: ['>0.25%', 'not ie 11', 'not op_mini all'],
+                    },
+                    modules: isTest ? 'auto' : false,
+                },
+            ],
+        ],
+    };
 };
-  
