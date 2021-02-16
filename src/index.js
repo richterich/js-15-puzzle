a9ba97d9512a4b5042a5bd5a7a7dfd8ac3363c3c
Alexander Richterich <richterich@yahoo.com>
2021-02-16 20:27:06 +0200

    Remove refresh background button.

--- src/index.js
+++ src/index.js
@@ -5,21 +5,11 @@
  */
 import Phaser from 'phaser';
 import GameConfig from './GameConfig';
-import applyBackground from './ApplyBackground';
 
 window.addEventListener('load', () => {
     bootGame();
-    let refresh = document.getElementById('refresh');
-    refresh.addEventListener('click', applyBackground);
 });
 
 function bootGame() {
-    applyBackground()
-        .then(() => {
-            new Phaser.Game(GameConfig);
-        })
-        .catch((err) => {
-            new Phaser.Game(GameConfig);
-            throw err;
-        });
+    new Phaser.Game(GameConfig);
 }
