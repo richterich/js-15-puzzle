db5e9009aab05a1577eb89dc3ea0fa440f41e1a8
Alexander Richterich <richterich@yahoo.com>
2021-02-26 18:54:19 +0200

    Add donation button.

--- src/index.js
+++ src/index.js
@@ -4,41 +4,45 @@
  * @license     {@link https://opensource.org/licenses/MIT|MIT License}
  */
 import '../styles/main.css';
+import donateImage from '../img/paypal_logo.png';
 import Phaser from 'phaser';
 import GameConfig from './GameConfig';
 
 window.addEventListener('load', () => {
     bootGame();
-
-    const repoStars = document.getElementById('repo-stars');
-    setInterval(() => {
-        repoStars.classList.remove('shake');
-        void repoStars.offsetWidth;
-        repoStars.classList.add('shake');
-    }, 90000);
-
-    const howPlay = document.getElementById('how-play');
-    const infoPanel = document.getElementById('info-panel');
-    const close = document.getElementsByClassName('close')[0];
-    howPlay.onclick = () => {
-        infoPanel.style.display = 'block';
-    };
-    close.onclick = () => {
-        infoPanel.style.display = 'none';
-    };
-    window.onclick = (event) => {
-        if (event.target == infoPanel) {
-            infoPanel.style.display = 'none';
-            return false;
-        }
-    };
-    setInterval(() => {
-        howPlay.classList.remove('shake');
-        void howPlay.offsetWidth;
-        howPlay.classList.add('shake');
-    }, 30000);
 });
 
 function bootGame() {
     new Phaser.Game(GameConfig);
 }
+
+const donate = document.getElementById('donate');
+donate.src = donateImage;
+
+const repoStars = document.getElementById('repo-stars');
+setInterval(() => {
+    repoStars.classList.remove('shake');
+    void repoStars.offsetWidth;
+    repoStars.classList.add('shake');
+}, 90000);
+
+const howPlay = document.getElementById('how-play');
+const infoPanel = document.getElementById('info-panel');
+const close = document.getElementsByClassName('close')[0];
+howPlay.onclick = () => {
+    infoPanel.style.display = 'block';
+};
+close.onclick = () => {
+    infoPanel.style.display = 'none';
+};
+window.onclick = (event) => {
+    if (event.target == infoPanel) {
+        infoPanel.style.display = 'none';
+        return false;
+    }
+};
+setInterval(() => {
+    howPlay.classList.remove('shake');
+    void howPlay.offsetWidth;
+    howPlay.classList.add('shake');
+}, 30000);
