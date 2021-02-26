e1ff1733000209f6e48768020233d10a7903a8a5
Alexander Richterich <richterich@yahoo.com>
2021-02-26 13:48:40 +0200

    Add "How to play" information.

--- src/index.js
+++ src/index.js
@@ -9,12 +9,34 @@ import GameConfig from './GameConfig';
 
 window.addEventListener('load', () => {
     bootGame();
+
     const repoStars = document.getElementById('repo-stars');
     setInterval(() => {
-        repoStars.classList.remove('rate');
+        repoStars.classList.remove('shake');
         void repoStars.offsetWidth;
-        repoStars.classList.add('rate');
+        repoStars.classList.add('shake');
     }, 90000);
+
+    const howPlay = document.getElementById('how-play');
+    const infoPanel = document.getElementById('info-panel');
+    const close = document.getElementsByClassName('close')[0];
+    howPlay.onclick = () => {
+        infoPanel.style.display = 'block';
+    };
+    close.onclick = () => {
+        infoPanel.style.display = 'none';
+    };
+    window.onclick = (event) => {
+        if (event.target == infoPanel) {
+            infoPanel.style.display = 'none';
+            return false;
+        }
+    };
+    setInterval(() => {
+        howPlay.classList.remove('shake');
+        void howPlay.offsetWidth;
+        howPlay.classList.add('shake');
+    }, 30000);
 });
 
 function bootGame() {
